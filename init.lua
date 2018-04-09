e9468cb7a531c2b6be58dc7e41a6e89ebabd7def
Ferrum Princeps I <ferrumprinceps@politician.com>
2018-04-09 15:22:26 -0500

    added node_restrictions.lua to dofile function

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local multiserver_path = minetest.get_modpath("multiserver")
 
+--servers
 dofile(multiserver_path.."/smp_server.lua")
 dofile(multiserver_path.."/creative_server.lua")
 dofile(multiserver_path.."/roleplay_server.lua")
@@ -9,3 +10,6 @@ dofile(multiserver_path.."/ctf_server.lua")
 dofile(multiserver_path.."/skywars_server.lua")
 dofile(multiserver_path.."/hunger_games_server.lua")
 dofile(multiserver_path.."/bedwars_server.lua")
+
+--functions
+dofile(multiserver_path.."/node_restrictions.lua")
