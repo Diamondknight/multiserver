f9acec33fe2b5d3ac43c474ae38458591918be6f
Ferrum Princeps I <ferrumprinceps@politician.com>
2018-04-10 14:29:14 -0500

    rewrote mod structure
    removed individual files for "servers" and decided to seperate by blocks, mapgen, function groups, etc

--- init.lua
+++ init.lua
@@ -1,15 +1,8 @@
 local multiserver_path = minetest.get_modpath("multiserver")
 
---servers
-dofile(multiserver_path.."/smp_server.lua")
-dofile(multiserver_path.."/creative_server.lua")
-dofile(multiserver_path.."/roleplay_server.lua")
-dofile(multiserver_path.."/anarchy_server.lua")
-dofile(multiserver_path.."/factions_server.lua")
-dofile(multiserver_path.."/ctf_server.lua")
-dofile(multiserver_path.."/skywars_server.lua")
-dofile(multiserver_path.."/hunger_games_server.lua")
-dofile(multiserver_path.."/bedwars_server.lua")
 
---functions
+dofile(multiserver_path.."/mapgen.lua")
+dofile(multiserver_path.."/blocks.lua")
+
+
 dofile(multiserver_path.."/node_restrictions.lua")
